5994871e4ccecc34b27138ad2c90af9266c8e326
dugsong <dugsong@d825174d-f523-0410-b672-7552a48fd9e8>
2006-12-13 17:09:07 +0000

    support channel specification as cmdline argument

--- trunk/test-pub.c
+++ trunk/test-pub.c
@@ -17,23 +17,26 @@ void
 send_msg(int fd, short event, void *arg)
 {
 	static int i;
+	char *channel = arg;
 	struct evbuffer *msg = evbuffer_new();
 
 	evbuffer_add_printf(msg, "<h1>hello world %d</h1>", i++);
-	evmsg_publish("flood", "text/html", msg);
+	evmsg_publish(channel, "text/html", msg);
 	event_add(&time_ev, &time_tv);
 }
 
 int
 main(int argc, char *argv[])
 {
+	char *channel = (argc > 1) ? argv[1] : "flood";
+	
 	putenv("EVENT_SHOW_METHOD=1");
 	event_init();
 
 	evmsg_open("localhost", 8080);
 	evmsg_set_auth(getenv("USER"), "foobar");
 	
-	evtimer_set(&time_ev, send_msg, NULL);
+	evtimer_set(&time_ev, send_msg, channel);
 	event_add(&time_ev, &time_tv);
 	
 	event_dispatch();
