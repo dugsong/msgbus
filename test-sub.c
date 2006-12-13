d00681d7303e2b101cd7d67f7b2aea3fa703ac1f
dugsong <dugsong@d825174d-f523-0410-b672-7552a48fd9e8>
2006-12-13 17:09:07 +0000

    support channel specification as cmdline argument

--- test-sub.c
+++ test-sub.c
@@ -19,12 +19,14 @@ recv_msg(const char *type, const char *sender, struct evbuffer *msg, void *arg)
 int
 main(int argc, char *argv[])
 {
+	char *channel = (argc > 1) ? argv[1] : "flood";
+	
 	putenv("EVENT_SHOW_METHOD=1");
 	event_init();
 
 	evmsg_open("localhost", 8080);
 
-	evmsg_subscribe("flood", NULL, NULL, recv_msg, NULL);
+	evmsg_subscribe(channel, NULL, NULL, recv_msg, NULL);
 	
 	event_dispatch();
 	
