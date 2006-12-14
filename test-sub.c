3024144f8e17af130d1a4c539d82caed0f16774c
dugsong <dugsong@d825174d-f523-0410-b672-7552a48fd9e8>
2006-12-14 02:25:51 +0000

    add options to set channel, number of subscriptions, and destination

--- test-sub.c
+++ test-sub.c
@@ -18,21 +18,50 @@ recv_msg(const char *channel, const char *type, const char *sender,
 	printf("%s %s %s [%s]\n", channel, type, sender, EVBUFFER_DATA(msg));
 }
 
+static void
+usage(void)
+{
+	fprintf(stderr, "usage: test-sub [-c channel] [-n num_subscriptions] "
+	    "[host [port]]\n");
+	exit(1);
+}
+
 int
 main(int argc, char *argv[])
 {
 	struct rlimit rlim = { RLIM_INFINITY, RLIM_INFINITY };
-	char *channel = (argc > 1) ? argv[1] : "flood";
-	int i;
+	char *server = NULL, *channel = "flood";
+	int i, num = 1, port = 0;
 
+	while ((i = getopt(argc, argv, "c:n:h?")) != -1) {
+		switch (i) {
+		case 'c':
+			channel = optarg;
+			break;
+		case 'n':
+			num = atoi(optarg);
+			break;
+		default:
+			usage();
+			break;
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (argc > 0) {
+		server = argv[0];
+		if (argc > 1)
+			port = atoi(argv[1]);
+	}
 	setrlimit(RLIMIT_NOFILE, &rlim);
 	
 	putenv("EVENT_SHOW_METHOD=1");
 	event_init();
 
-	evmsg_open(NULL, 0);
+	evmsg_open(server, port);
 
-	for (i = 0; i < 1000; i++)
+	for (i = 0; i < num; i++)
 		evmsg_subscribe(channel, NULL, NULL, recv_msg, NULL);
 	
 	event_dispatch();
