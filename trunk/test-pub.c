1ba629f7e78dd8fffa9edadd2c677607e4721070
dugsong <dugsong@d825174d-f523-0410-b672-7552a48fd9e8>
2006-12-14 02:17:58 +0000

    add options to specify message rate, channel, destination

--- trunk/test-pub.c
+++ trunk/test-pub.c
@@ -10,9 +10,9 @@
 #include "event.h"
 #include "evmsg.h"
 
-struct event send_ev, stats_ev;
-struct timeval send_tv = { 0, 10000 }, stats_tv = { 5, 0 };
-int cnt;
+struct event	send_ev, stats_ev;
+struct timeval	send_tv = { 1, 0 }, stats_tv = { 5, 0 };
+int		send_cnt;
 
 void
 send_msg(int fd, short event, void *arg)
@@ -23,27 +23,58 @@ send_msg(int fd, short event, void *arg)
 
 	evbuffer_add_printf(msg, "<h1>hello world %d</h1>", i++);
 	evmsg_publish(channel, "text/html", msg);
-	cnt++;
+	send_cnt++;
 	event_add(&send_ev, &send_tv);
 }
 
 void
 print_stats(int fd, short event, void *arg)
 {
-	printf("%.1f messages/s\n", ((float)cnt) / stats_tv.tv_sec);
-	cnt = 0;
+	printf("%.1f msgs/s\n", ((float)send_cnt) / stats_tv.tv_sec);
+	send_cnt = 0;
 	event_add(&stats_ev, &stats_tv);
+}
+
+static void
+usage(void)
+{
+	fprintf(stderr, "usage: test-pub [-c channel] [-r msgs_per_sec] "
+	    "[host [port]]\n");
+	exit(1);
 }
 
 int
 main(int argc, char *argv[])
 {
-	char *channel = (argc > 1) ? argv[1] : "flood";
-	
+	char *server = NULL, *channel = "flood";
+	int c, port = 0;
+
+	while ((c = getopt(argc, argv, "c:r:h?")) != -1) {
+		switch (c) {
+		case 'c':
+			channel = optarg;
+			break;
+		case 'r':
+			send_tv.tv_sec = 0;
+			send_tv.tv_usec = 1000000 / atoi(optarg);
+			break;
+		default:
+			usage();
+			break;
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (argc > 0) {
+		server = argv[1];
+		if (argc > 1)
+			port = atoi(optarg);
+	}
 	putenv("EVENT_SHOW_METHOD=1");
 	event_init();
 
-	evmsg_open(NULL, 0);
+	evmsg_open(server, port);
 	evmsg_set_auth(getenv("USER"), "foobar");
 	
 	evtimer_set(&send_ev, send_msg, channel);
