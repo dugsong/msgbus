087c5a632779f01a5273210307780060f42d255b
dugsong <dugsong@d825174d-f523-0410-b672-7552a48fd9e8>
2006-12-20 07:51:08 +0000

    require URL or channel

--- trunk/msgdump.py
+++ trunk/msgdump.py
@@ -5,7 +5,7 @@
 import httplib, optparse, re, sys, time, urllib
 
 def main():
-    op = optparse.OptionParser(usage='%prog [OPTIONS] [URL]')
+    op = optparse.OptionParser(usage='%prog [OPTIONS] URL|channel')
     op.add_option('-v', dest='verbose', help='enable verbose output',
                   action='store_true')
     op.add_option('-s', dest='sender',
@@ -14,12 +14,17 @@ def main():
                   help='subscribe to messages of this type')
     opts, args = op.parse_args(sys.argv[1:])
     if not args:
-        args.append('http://localhost:8888/msgbus/flood')
+        op.error('missing URL or channel')
 
-    l = [ args[0], '%s' % urllib.urlencode(dict(filter(lambda x: x[1],
-            (('sender', opts.sender), ('type', opts.type))))) ]
-    url = '?'.join(filter(None, l))
+    if not args[0].startswith('http'):
+        url = 'http://localhost:8888/msgbus/%s' % args[0]
+    else:
+        url = args[0]
 
+    if opts.sender or opts.type:
+        url += '?' + urllib.urlencode(dict(filter(lambda x: x[1],
+            (('sender', opts.sender), ('type', opts.type)))))
+    
     f = urllib.urlopen(url)
     hdrs = f.info()
     mp = re.compile('^multipart/.*boundary="?([^;"\n]*)', re.I|re.S)
