121be92ed324072bc7d01683ed4c5a9be5fdd6f6
dugsong <dugsong@d825174d-f523-0410-b672-7552a48fd9e8>
2006-12-20 07:47:06 +0000

    refactor

--- msgsend.py
+++ msgsend.py
@@ -2,41 +2,46 @@
 
 # $Id$
 
-import base64, httplib, optparse, os, sys
+import base64, httplib, optparse, sys, urlparse
 
 def main():
-    op = optparse.OptionParser(usage='%prog [OPTIONS] CHANNEL')
-    op.add_option('-d', dest='address', default='127.0.0.1',
-                  help='specify alternate destination address')
-    op.add_option('-p', dest='port', default=8888, type='int',
-                  help='specify alternate destination port')
-    op.add_option('-f', dest='filename', help='specify file to data from')
-    op.add_option('-a', dest='auth', help='username:password to send')
+    op = optparse.OptionParser(usage='%prog [OPTIONS] URL|channel')
+    op.add_option('-f', dest='filename',
+                  help='specify input file (default stdin)')
     op.add_option('-t', dest='type', help='content-type (default text/html)',
                   default='text/html')
     opts, args = op.parse_args(sys.argv[1:])
     if not args:
-        op.error('no channel specified')
+        op.error('missing URL or channel')
+
+    if not args[0].startswith('http'):
+        url = 'http://localhost:8888/msgbus/%s' % args[0]
+    else:
+        url = args[0]
         
-    # Read message data.
+    p = urlparse.urlsplit(url)
+    if not p.path.startswith('/msgbus/'):
+        raise ValueError, 'invalid msgbus URL: %s' % url
+    
     f = opts.filename and open(opts.filename) or sys.stdin
     buf = f.read()
+    hdrs = {
+        'Content-Type':opts.type,
+        'Content-Length':str(len(buf)),
+        'Connection':'close'
+        }
+    if p.username and p.password:
+        s = '%s:%s' % (p.username, p.password)
+        hdrs['Authorization'] = 'Basic %s' % base64.encodestring(s.strip())
 
-    # Send message.
-    channel = ' '.join(args)
-    uri = '/msgbus/' + ' '.join(args)
-    hdrs = { 'Content-Type':opts.type, 'Content-Length':str(len(buf)),
-             'Connection':'close' }
-    if opts.auth:
-        hdrs['Authorization'] = 'Basic %s' % \
-            base64.encodestring(opts.auth).strip()
-
-    conn = httplib.HTTPConnection('%s:%s' % (opts.address, opts.port))
-    conn.request('POST', uri, buf, hdrs)
+    if p.scheme == 'https':
+        conn = httplib.HTTPSConnection('%s:%s' % (p.hostname, p.port or 4444))
+    else:
+        conn = httplib.HTTPConnection('%s:%s' % (p.hostname, p.port or 8888))
+    conn.request('POST', p.path, buf, hdrs)
     res = conn.getresponse()
     if res.status != 204:
-        print res.status, res.reason
-        print res.read()
+        raise RuntimeError, (res.status, res.reason)
     conn.close()
 
 if __name__ == '__main__':
